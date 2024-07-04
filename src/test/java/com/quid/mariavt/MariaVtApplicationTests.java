2b039535a78adc8865527c793ca24ec3cfa9b29e
QuiD-0 <wodnd101@naver.com>
2024-07-04 10:56:14 +0900

    project init

---
+++ src/test/java/com/quid/mariavt/MariaVtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quid.mariavt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MariaVtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

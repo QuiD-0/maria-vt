6bcb22fd477a9bf846540832161bad3a7ac4a165
quid-0 <wodnd101@naver.com>
2024-07-05 17:33:12 +0900

    테스트 서버 분리

---
+++ src/test/java/com/quid/mariavt/without_db/TestingControllerTest.java
@@ -0,0 +1,10 @@
+package com.quid.mariavt.without_db;
+
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.TestMethodOrder;
+
+
+@TestMethodOrder(value = OrderAnnotation.class)
+class TestingControllerTest {
+
+}

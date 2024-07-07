85cb09125583e44e69a3f212ff5a4cd6e818644a
quid-0 <wodnd101@naver.com>
2024-07-07 21:26:07 +0900

    플랫폼 수정

--- platform/src/main/java/com/quid/platform/simple/SimpleRepository.java
+++ platform/src/main/java/com/quid/platform/simple/SimpleRepository.java
@@ -1,6 +1,5 @@
 package com.quid.platform.simple;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
 
@@ -9,10 +8,13 @@ public interface SimpleRepository {
     String find500ms();
 
     @Repository
-    @RequiredArgsConstructor
     class SimpleRepositoryImpl implements SimpleRepository {
 
         private final JdbcClient jdbcClient;
+
+        public SimpleRepositoryImpl(JdbcClient jdbcClient) {
+            this.jdbcClient = jdbcClient;
+        }
 
         @Override
         public String find500ms() {
